3394009e38eef788647414a6d0a9566f20b3d63e
Ever <evertonxdoj@gmail.com>
2023-05-13 19:47:06 -0300

    indexOf and ChooseOption created

--- DragonText/Mechanics.h
+++ DragonText/Mechanics.h
@@ -1,8 +1,42 @@
 #pragma once
 
+int indexOf(vector<int> array_, int valueToFind) {
+	for (int i = 0; i < array_.size(); i++) {
+		if (array_[i] == valueToFind) {
+			return i;
+		}
+	}
+	return -1;
+}
+int indexOf(vector<char> array_, int valueToFind) {
+	for (int i = 0; i < array_.size(); i++) {
+		if (array_[i] == valueToFind) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 int RollDice(int maxValue) {
 	// set random seed
 	srand(time(NULL));
 	int result = (rand() % maxValue) + 1;
 	return result;
+}
+
+int ChooseOption(vector<int> options) {
+	int option;
+	while (true) {
+		if (std::cin >> option) {
+			int optionPosition = indexOf(options, option);
+			if (optionPosition != -1) {
+				return options[optionPosition];
+			}
+		}
+		else {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		std::cout << "Invalid answer, try again...\n";
+	}
 }
